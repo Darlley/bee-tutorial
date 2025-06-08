5d4cf44ce4dc47aafb7e38985792826d3179f50f
Darlley <systembrtt@gmail.com>
2025-06-08 17:01:24 -0400

    fix: position

--- app.js
+++ app.js
@@ -64,21 +64,21 @@ let arrPositionModel = [
     animation: 0
   },
   {
-    id: "intro",
+    id: "description",
     position: { x: 1, y: -1, z: -5 },
     rotation: { x: 0.5, y: -0.5, z: 0 },
     animation: 0
   },
   {
-    id: "description",
+    id: "intro",
     position: { x: -1, y: -1, z: -5 },
     rotation: { x: 0, y: 0.5, z: 0 },
     animation: 0
   },
   {
     id: "contact",
-    position: { x: 0.8, y: -1, z: 0 },
-    rotation: { x: 0.3, y: -0.5, z: 0 },
+    position: { x: -1, y: -1, z: -5 },
+    rotation: { x: 0, y: 0.5, z: 0 },
     animation: 2
   },
 ];
